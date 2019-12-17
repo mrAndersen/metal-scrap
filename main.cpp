84f2a92bb8553c8d6119dd3f6d74f2d0708168c4
Armen Alaverdyan <mrandersen2004@yandex.ru>
2019-12-17 19:00:50 +0300

    corrections

--- main.cpp
+++ main.cpp
@@ -4,37 +4,10 @@
 #include <regex>
 
 
-class CpuMetrics {
-public:
-    float load1 = 0;
-    float load5 = 0;
-    float load15 = 0;
-    int processCount = 0;
-    float parsingTime = 0;
-
-    std::string toString() {
-        auto string = fmt::format(
-                "load1={},load5={},load15={},ps={},time={}ms",
-                this->load1,
-                this->load5,
-                this->load15,
-                this->processCount
-        );
-
-        return string;
-    }
-};
-
 class Collector {
 protected:
-    std::string procPath = "/proc";
-    std::regex cpu_r;
-
-public:
-    Collector() {
-        this->cpu_r = R"((\d+.\d+)\s(\d+.\d+)\s(\d+.\d+)\s\d+\/(\d+)\s\d+)";
-
-    }
+    std::string procPath;
+    std::regex regex;
 
 public:
     std::string readFile(const std::string &path) {
@@ -43,24 +16,54 @@ public:
 
         return string;
     }
+};
 
 
-    CpuMetrics collectCpu() {
-        auto path = this->procPath + "/loadavg";
-        auto data = this->readFile(path);
+class CpuMetrics : public Collector {
+public:
+    float load1 = 0;
+    float load5 = 0;
+    float load15 = 0;
+    int processCount = 0;
+    float parsingTime = 0;
+
+    CpuMetrics() {
+        this->regex = std::regex(R"((\d+.\d+)\s(\d+.\d+)\s(\d+.\d+)\s\d+\/(\d+)\s\d+)");
+        this->procPath = "/proc/loadavg";
+    }
+
+    std::string toString() {
+        std::string result = {};
+
+        if (this->load1 == 0) {
+            return result;
+        }
+
+        auto string = fmt::format(
+                "load1={},load5={},load15={},ps={},time={}ms",
+                this->load1,
+                this->load5,
+                this->load15,
+                this->processCount,
+                this->parsingTime
+        );
+
+        return string;
+    }
+
+    void collect() {
+        auto data = this->readFile(this->procPath);
 
         std::smatch match;
-        std::regex_search(data, match, this->cpu_r);
+        std::regex_search(data, match, this->regex);
 
-        CpuMetrics cpuMetrics;
-        cpuMetrics.load1 = std::stof(match[1]);
-        cpuMetrics.load5 = std::stof(match[2]);
-        cpuMetrics.load15 = std::stof(match[3]);
-        cpuMetrics.processCount = std::stoi(match[4]);
-
-        return cpuMetrics;
+        this->load1 = std::stof(match[1]);
+        this->load5 = std::stof(match[2]);
+        this->load15 = std::stof(match[3]);
+        this->processCount = std::stoi(match[4]);
     }
 };
+
 
 struct MemoryMetrics {
 
@@ -68,13 +71,11 @@ struct MemoryMetrics {
 
 
 int main() {
-    std::string procPath = "/proc";
-    Collector collector;
 
 
-    auto cpu = collector.collectCpu();
-    printf(cpu.toString().c_str());
+    auto cpu = CpuMetrics();
+    cpu.collect();
 
-
+    printf("%s", cpu.toString().c_str());
     return 0;
 }
