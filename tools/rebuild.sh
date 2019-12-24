626de5f326eb8a89dc42707adad5750824a90f76
Armen Alaverdyan <mrandersen2004@yandex.ru>
2019-12-24 18:28:31 +0300

    remove alpine docker

--- tools/rebuild.sh
+++ tools/rebuild.sh
@@ -1,4 +1,9 @@
-docker build . -t mrandersen7/metal \
-  && docker rm -f metal || true \
-  && docker create --name metal --network host -v /proc:/hostproc --privileged metal_image \
-  && docker start metal
+version=$1
+
+if [[ -z "$version" ]]; then
+    echo -e "Enter version"
+    exit 1
+fi
+
+docker build . -t mrandersen7/metal:"${version}" \
+  && docker push mrandersen7/metal:"${version}"
